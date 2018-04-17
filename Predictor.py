96d2cc6f0b91f7a510296ee31a78c3ac28565725
mateozabala <mzabala1@eafit.edu.co>
2018-04-17 18:46:59 -0500

    Adding new class Predictor, modifying the other classes

---
+++ Predictor.py
@@ -0,0 +1,92 @@
+import numpy  #permite usar arrays
+from sklearn import tree  #permite usar arboles de desicion
+
+from sklearn.tree import export_graphviz #esta libreria nos ofrece la opcion de graficar el arbol
+import graphviz
+import interpretar as inter  
+import matplotlib.pyplot as plt 
+import numpy as np  #nos ofrece la opcion de crear vectores y matrices
+import sys
+
+
+red=MLPClassifier(max_iter=(10000), hidden_layer_sizes=(1000,1000))
+clasif = tree.DecisionTreeClassifier() #crea un variable con todos los atributos y metodos del arbol clasificador
+z=['U1C','U2C','U3C','U4C','U1T','U2T','U3T','U4T','U1I','U2I','U3I','U4I','U1M','U2M','U3M','U4M']
+
+def cargarArchivos():
+	entidad=[]
+	atributos=[]
+	with open(sys.argv[1]) as f:  #abre el archivo donde estan los 60 datos por estudiante
+		for line in f:  # por cada iteracion carga en f linea a linea el archivo
+			cont = 0
+			temp = line.strip()  #quita especios adelante y atras de cada linea
+			array = temp.split(',')  # separa la lina por ',' y cada uno de estas particiones las guarda como elementos en un arreglo
+			entidad.append(array[0:11]) # toma los primeros datos los cuales corresponden a datos personales
+			atributos.append(array[11:len(array)]) #toma los datos resptantes correspondientes al desempeno
+	global clasif
+	clasif= clasif.fit(entidad,atributos) # entrena el modelos con 2 matrices dadas (de 11 y 49 datos)
+	imprimir()
+
+def entrenar(entrada,salida):
+	global clasif
+	clasif= clasif.fit(entrada,salida) # entrena el modelos con 2 matrices dadas (de 11 y 49 datos)
+
+def predecir(datoPredecir):
+	global clasif
+	return clasif.predict([datoPredecir]) #predice desempeno en base a un dato entrada
+
+def interpretar(x):	#interpreta la prediccion obtenida, la resive como parametro
+	i=2
+	y=[]
+	while i < len(x[0]):
+		if x[0][i]> 0.0:
+			y.append('-')
+		else:
+			y.append('Reforzar')
+		i=i+3
+	i=0
+	while i < len(z): 
+		print (y[i],z[i])
+		i=i+1
+
+def probar(entrada,salida): #dadas 2 matrices (20% de los datos) prueba que tan preciso es el modelo
+	i=0
+	cantDatosIguales=0
+	for dato in entrada:
+		array=predecir(dato)
+		cantDatosIguales=cantDatosIguales + probarIgualdad(array[0],salida[i])
+		i=i+1
+	size=len(salida)*48
+	presicion=(cantDatosIguales+0.0)/(size+0.0)
+	return presicion		
+
+def probarIgualdad(array1,array2): #este metodo sirve como apoyo al metodo probar compara 2 arrays
+	cantDatosIgu=0
+	i=0
+	while i < len(array1):
+		a=int(array1[i])
+		b=int(array2[i])
+		if a == b:
+			cantDatosIgu=cantDatosIgu+1
+		#else:
+			#print(array1[i],array2[i])
+		i=i+1	
+	return cantDatosIgu
+
+def imprimir():
+	global clasif
+	names=['genero','edad','grado','ciencia','tecnologia',
+		'ingenieria','metematica','estrato','madre','padre','hermanos']
+
+	export_graphviz(clasif,out_file='arbol.dot',feature_names=names, 
+		class_names=['l','m','n','o','p','q','r','s','t','u','v'],impurity=False,filled=True)	
+	with open('arbol.dot') as f:
+		dot_graph=f.read()
+	graphviz.Source(dot_graph)
+	print("termino")
+
+	plt.barh(range(11),clasif.feature_importances_)
+	plt.yticks(np.arange(11),names)
+	plt.xlabel('Importacia de la variables')
+	plt.ylabel('Variable')
+	plt.show()
--- README.md
+++ README.md
@@ -8,3 +8,36 @@ Science, Technology, Engineering, Maths.
 VR+Artificial Intelligence application developed for the subject integrative project 2.
 
 This repository only contains the Artificial Intelligence module made with python.
+
+THe project uses its main class Predictor, please ignore the others and you must run it with the probar class
+
+
+Caren:
+	takes a file with 60 characters, example:
+	12,34,223,212,1,0,34,131,123, ... ,3
+
+	then it divides the collected data in two groups, 
+	the first contains 11 characters and the other 49.
+	it generates 2 files
+	(trainingData=49 characters y LearnData=11 characters)
+	in those files it writes the data like this
+	[[12,34,223,212],[1,0,34,131]]
+
+Helen: 
+	
+	this class get both files generated with Caren.
+	with those characters it train the model
+	then in an infinite cycle it receives 11 data and predicts the performance
+	based on this data.
+
+Predictor:
+	This class has the following methods: 
+	train: train the model with 2 given arrays of 11 and 49 characters
+	predict: predicts the performance of a student based on 11 personal characters.
+	interpret: interprets the predicted obtained.
+	try: receive 20% of the data and calculate how accurate the model is
+
+	to see the generated tree this command must be executed:
+	$ python Probar.py
+	$ dot -Tpng tree.dot -o tree.png
+	$ dot -Tps tree.dot -o tree.ps
